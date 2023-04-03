f8b774c89d738ce7bd56725fc0d2248e285db47d
FrozenGhost91 <72307776+FrozenGhost91@users.noreply.github.com>
2023-04-03 13:58:14 +0200

    unique filename function

--- python/CanLogger.py
+++ python/CanLogger.py
@@ -16,4 +16,17 @@ def timestamp(format_=None):
             time=time.strftime("%Y%m%d_%H%M%S")
          case others:
             time=time.strftime("%H:%M:%S:%f")
-     return time       
+     return time
+
+
+def createNewFilename(name,ending):
+    fileNumber=0
+    while(exists(name+"_"+str(fileNumber)+"."+ending)):
+        fileNumber+=1
+    return name+"_"+str(fileNumber)+"."+ending
+
+
+
+
+
+
