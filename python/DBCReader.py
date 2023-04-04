65d12fb4b7b736ae873fc176ed86bdc63a54bb6e
FrozenGhost91 <72307776+FrozenGhost91@users.noreply.github.com>
2023-04-04 10:07:08 +0200

    open files added

--- python/DBCReader.py
+++ python/DBCReader.py
@@ -1,3 +1,10 @@
 # this file is used to read in a dbc file
 # therefore the data that gets send over can can be interpreted with this file
 
+# slect a dbcfile and a datalogging file
+dbcFile=open("CANoe_C23.dbc","r")
+dataFile=open("testlogging.txt","r")
+
+
+
+
